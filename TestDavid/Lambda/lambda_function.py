3f77f420d7767d3595c2080c203d279b87d1019c
codyvi <davidcantumartinez@gmail.com>
2019-10-04 15:46:04 -0500

    Changes made

--- TestDavid/Lambda/lambda_function.py
+++ TestDavid/Lambda/lambda_function.py
@@ -46,11 +46,11 @@ class HelloWorldIntentHandler(AbstractRequestHandler):
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
         speak_output = "Hola Mundo!"
-
+        test  = "Recuerda que me puedes decir cualquier cosa"
         return (
             handler_input.response_builder
                 .speak(speak_output)
-                # .ask("add a reprompt if you want to keep the session open for the user to respond")
+                .ask(test)
                 .response
         )
 
