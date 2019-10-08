0e13fe626783ab355d7b33f8b8366d09009399af
codyvi <davidcantumartinez@gmail.com>
2019-10-08 15:25:02 -0500

    Deleting test variables

--- TestDavid/Lambda/test.py
+++
@@ -1,6 +0,0 @@
-import random
-
-compliments = [line for line in open('compliments.txt')]
-compliment = compliments[random.randint(0, len(compliments)-1)]
-speak_output = compliment
-print(speak_output)
