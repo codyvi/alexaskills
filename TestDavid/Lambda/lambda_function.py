4cd3093e29255ef16cbf20ecddf8a86cd094e377
codyvi <davidcantumartinez@gmail.com>
2019-10-14 12:48:10 -0500

    Made it Work!

--- TestDavid/Lambda/lambda_function.py
+++ TestDavid/Lambda/lambda_function.py
@@ -15,6 +15,29 @@ from ask_sdk_core.handler_input import HandlerInput
 
 from ask_sdk_model import Response
 
+from tinydb import TinyDB, Query
+
+def Convert(string): 
+    li = list(string.split(" ")) 
+    return li 
+
+
+db = TinyDB('/tmp/db.json')
+Name = Query()
+yeet = db.search(Name.name == 'david')
+
+
+
+#Convierto a string la lista con un atributo
+str1 = ''.join(str(e) for e in yeet)
+# print(str1)
+#conviero el string a lista pero ya separada y pues ya nada mas es de saber en que indice esta el atributo  
+newli = Convert(str1)
+
+s = newli[len(newli) - 1]
+#le quito los '{' del json
+s = s.translate({ord(i): None for i in '"}'})
+s = s.translate({ord(i): None for i in "'"})
 
 
 
