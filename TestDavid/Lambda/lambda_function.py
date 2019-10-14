edfe4548b335f2ffb572fe8190eb4dbb6529a0f0
codyvi <davidcantumartinez@gmail.com>
2019-10-14 12:57:22 -0500

    Let's see if these works

--- TestDavid/Lambda/lambda_function.py
+++ TestDavid/Lambda/lambda_function.py
@@ -22,24 +22,6 @@ def Convert(string):
     return li 
 
 
-db = TinyDB('/tmp/db.json')
-Name = Query()
-yeet = db.search(Name.name == 'david')
-
-
-
-#Convierto a string la lista con un atributo
-str1 = ''.join(str(e) for e in yeet)
-# print(str1)
-#conviero el string a lista pero ya separada y pues ya nada mas es de saber en que indice esta el atributo  
-newli = Convert(str1)
-
-s = newli[len(newli) - 1]
-#le quito los '{' del json
-s = s.translate({ord(i): None for i in '"}'})
-s = s.translate({ord(i): None for i in "'"})
-
-
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -109,7 +91,23 @@ class DineroIntentHandler(AbstractRequestHandler):
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
-        s = '8000'
+        db = TinyDB('./tmp/db.json')
+        Name = Query()
+        yeet = db.search(Name.name == 'david')
+
+
+
+        #Convierto a string la lista con un atributo
+        str1 = ''.join(str(e) for e in yeet)
+        # print(str1)
+        #conviero el string a lista pero ya separada y pues ya nada mas es de saber en que indice esta el atributo  
+        newli = Convert(str1)
+
+        s = newli[len(newli) - 1]
+        #le quito los '{' del json
+        s = s.translate({ord(i): None for i in '"}'})
+        s = s.translate({ord(i): None for i in "'"})
+
         speak_output = ("David, tu dinero disponible es: " +s)
         test  = "Recuerda que me puedes decir cualquier cosa"
         return (
