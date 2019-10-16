0407e64d96b44c8247e2019b833b85ddd0848995
codyvi <davidcantumartinez@gmail.com>
2019-10-16 08:19:11 -0500

    Trying to use DynamoDB

--- TestDavid/Lambda/lambda_function.py
+++ TestDavid/Lambda/lambda_function.py
@@ -15,13 +15,6 @@ from ask_sdk_core.handler_input import HandlerInput
 
 from ask_sdk_model import Response
 
-from tinydb import TinyDB, Query
-
-def Convert(string): 
-    li = list(string.split(" ")) 
-    return li 
-
-
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -91,22 +84,7 @@ class DineroIntentHandler(AbstractRequestHandler):
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
-        # db = TinyDB('./tmp/db.json')
-        # Name = Query()
-        # yeet = db.search(Name.name == 'david')
 
-
-
-        # #Convierto a string la lista con un atributo
-        # str1 = ''.join(str(e) for e in yeet)
-        # # print(str1)
-        # #conviero el string a lista pero ya separada y pues ya nada mas es de saber en que indice esta el atributo  
-        # newli = Convert(str1)
-
-        # s = newli[len(newli) - 1]
-        # #le quito los '{' del json
-        # s = s.translate({ord(i): None for i in '"}'})
-        # s = s.translate({ord(i): None for i in "'"})
         s = 80000
         speak_output = ("David, tu dinero disponible es: " +s)
         test  = "Recuerda que me puedes decir cualquier cosa"
