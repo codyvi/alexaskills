9cf1c3ed1cc2e3e486b11cae674591a73ce1e38d
codyvi <davidcantumartinez@gmail.com>
2020-01-17 12:35:04 -0600

    Deleted hello world intent

--- Viajes/lambda(Not touch)/index.js
+++ Viajes/lambda(Not touch)/index.js
@@ -35,19 +35,7 @@ const NombreHandler = {
 };
 
 
-const HelloWorldIntentHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
-    },
-    handle(handlerInput) {
-        const speakOutput = 'Hello World!';
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
-            .getResponse();
-    }
-};
+
 const HelpIntentHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
