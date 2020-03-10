de4d16aafe12a99976c46b28ac4c3ae5d9faf29d
David <davidcantumartinez@gmail.com>
2020-03-09 20:11:11 -0600

    Post methods in alexa

--- Ejercicio/apiUtil.js
+++ Ejercicio/apiUtil.js
@@ -43,7 +43,72 @@ module.exports.UpdateHora = async function UpdateHora(slotUsuario, hora){
 		.catch( err => {
 			console.log( err );
 		})
-  
+}
+
+module.exports.UpdateHoraFinal = async function UpdateHoraFinal(slotUsuario, horaFinal){
+	var id  = slotUsuario;
+	var tFin = horaFinal;
+
+	let url2 = 'https://ejercicio-api.herokuapp.com/api/EjercicioAn2'
+	let settings2 = {
+		method: 'POST',
+		body: JSON.stringify({
+			id: id,
+			tiempoFinal: parseInt(tFin)
+		}),
+		headers: {
+			'Content-Type': 'application/json'
+		},
+	}
+
+	fetch(url2, settings2)
+	.then( response => {
+		if ( response.ok ){
+			return response.json();
+		}
+
+		throw new Error ( response.statusText );
+	})
+	.then( responseJSON => {
+
+		console.log(responseJSON);
+	})
+	.catch( err => {
+		console.log( err );
+	})
+}
+
+module.exports.UpdateNivel = async function UpdateNivel(slotUsuario, nivel){
+		var id  = slotUsuario;
+		var nIvel = nivel;
+
+		let url2 = '/api/EjercicioAn3'
+		let settings2 = {
+			method: 'POST',
+			body: JSON.stringify({
+				id: id,
+				nivel: parseInt(nIvel)
+			}),
+			headers: {
+				'Content-Type': 'application/json'
+			},
+		}
+
+		fetch(url2, settings2)
+		.then( response => {
+			if ( response.ok ){
+				return response.json();
+			}
+
+			throw new Error ( response.statusText );
+		})
+		.then( responseJSON => {
+
+			console.log(responseJSON);
+		})
+		.catch( err => {
+			console.log( err );
+		})
 }
 
 
