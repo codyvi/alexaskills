0068f77d5c64fe9d5fba2574b0f6ee030dfb0b55
David <davidcantumartinez@gmail.com>
2020-05-12 17:01:57 -0500

    Adding sound directives

--- Ejercicio/index.js
+++ Ejercicio/index.js
@@ -71,17 +71,14 @@
         async handle(handlerInput) {
             const prevSession = handlerInput.attributesManager.getSessionAttributes();
             let name = prevSession.Nombre;
-            var audioFile = '<audio src="https://audios-de-alexa.s3.amazonaws.com/Basico+1+.mp3" />';
-            var audioFile2 = '<audio src="https://audios-de-alexa.s3.amazonaws.com/Basico+2.mp3" />';
-            var audioFile3 = '<audio src="https://audios-de-alexa.s3.amazonaws.com/Basico+3.mp3" />';
+            var audioFile =  "https://audios-de-alexa.s3.amazonaws.com/Basico+VT+GaD.mp3";
 
-            var audioMedio1 = '<audio src="https://audios-de-alexa.s3.amazonaws.com/VT+medio+1.mp3" />';
-            var audioMedio2 = '<audio src="https://audios-de-alexa.s3.amazonaws.com/VT+medio+2.mp3" />';
-            var audioMedio3 = '<audio src="https://audios-de-alexa.s3.amazonaws.com/VT+medio+3.mp3" />';
 
-            var audioDificil1 = '<audio src="https://audios-de-alexa.s3.amazonaws.com/VT+Avanzado+GaD+1.mp3" />'; 
-            var audioDificil2 = '<audio src="https://audios-de-alexa.s3.amazonaws.com/VT+Avanzado+GaD+2.mp3" />';
-            var audioDificil3 = '<audio src="https://audios-de-alexa.s3.amazonaws.com/VT+Avanzado+GaD+3.mp3" />';
+            var audioMedio1 = "https://audios-de-alexa.s3.amazonaws.com/VT+medio+GaD.mp33";
+
+
+            var audioDificil1 = "https://audios-de-alexa.s3.amazonaws.com/VT+Avanzado+GaD+1.mp3";
+     
 
 
             var speakOutput = '';
@@ -91,19 +88,34 @@
             var minutos = 0;
             if(nivel === 1)
             {
-                speakOutput = `Claro ${name}, iniciando nivel Basico, ${audioFile}  ${audioFile2} ${audioFile3}. Recuerda que puedes conocer tus datos de la semana diciendo ¿Como me fue en la semana?`;
+                let direccion = audioFile
+                console.log(`${direccion}`);
+                handlerInput.responseBuilder
+                .addDirective({
+                type: "AudioPlayer.Play",
+                playBehavior: "REPLACE_ALL",
+                audioItem: {
+                stream: {     
+                url: `${direccion}`,
+                token: "0",
+                offsetInMilliseconds: 0
+                    }
+                }
+        }); 
+                
+                speakOutput = `Claro ${name}, iniciando nivel Basico,. Recuerda que puedes conocer tus datos de la semana diciendo ¿Como me fue en la semana?`;
                 minutos = 5;
             }
 
             else if(nivel === 2)
             {
-                speakOutput = `Claro ${name}, iniciando nivel Intermedio, ${audioMedio1}  ${audioMedio2} . Recuerda que puedes conocer tus datos de la semana diciendo ¿Como me fue en la semana?`;
+                speakOutput = `Claro ${name}, iniciando nivel Intermedio, . Recuerda que puedes conocer tus datos de la semana diciendo ¿Como me fue en la semana?`;
                 minutos = 7;
             }
             
             else if(nivel === 3)
             {
-                speakOutput = `Claro ${name}, iniciando nivel Avanzado, ${audioDificil1}  ${audioDificil2} . Recuerda que puedes conocer tus datos de la semana diciendo ¿Como me fue en la semana?`;
+                speakOutput = `Claro ${name}, iniciando nivel Avanzado,  . Recuerda que puedes conocer tus datos de la semana diciendo ¿Como me fue en la semana?`;
                 minutos = 9;
             }
 
