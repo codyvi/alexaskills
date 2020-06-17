aab7419e05084153062316a911304bc34af9a98a
David <davidcantumartinez@gmail.com>
2020-06-17 12:14:56 -0500

    Forgot to export the handle

--- PresupuestoPersonal/lambda/index.js
+++ PresupuestoPersonal/lambda/index.js
@@ -292,6 +292,7 @@
             LaunchRequestHandler,
             NombreHandler,
             FollowUpUnoHandler,
+            FollowUpDosHandler,
             HelpIntentHandler,
             CancelAndStopIntentHandler,
             SessionEndedRequestHandler,
